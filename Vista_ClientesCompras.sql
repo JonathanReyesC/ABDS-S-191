3d22097d9d8a92545f11257f4bf8521d6b8a23a4
JonathanReyesC <Jonathancarbajal999@gmail.com>
2024-06-10 18:46:45 -0600

    Vistas Practica 7

---
+++ Vista_ClientesCompras.sql
@@ -0,0 +1,22 @@
+
+-- Crear la vista
+CREATE VIEW VISTA_ClientesCompras AS
+SELECT 
+    c.id_cliente, 
+    c.nombre AS CLIENTE, 
+    c.correo_electronico, 
+    cm.id_compra, 
+    cm.fecha_compra, 
+    cm.total
+FROM 
+    Clientes c
+LEFT JOIN 
+    Compras cm ON c.id_cliente = cm.id_cliente;
+
+
+SELECT * FROM VISTA_ClientesCompras;
+
+SELECT * FROM  Vista_ComicsInventario;
+
+SELECT * FROM  Clientes_ComicsCompras;
+
